03f9ad92208fa16475f0bdc5ed658d769af1a36c
wangyipeng0724 <15356130934@163.com>
2022-04-11 15:33:31 +0800

    update

---
+++ exercise/car.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+class Car {
+private:
+	string brand;  // 汽车品牌
+	string model;  // 汽车模型
+	int year;      // 生产年份
+
+public:
+	// 构造函数
+	Car(string brand, string model, int year);
+
+	void display(string brand, string model, int year);
+};
+
+Car::Car(string brand, string model, int year) 
+	:brand(brand), model(model), year(year) {
+	cout << "该汽车的信息是：" << endl;
+}
+
+void Car::display(string brand, string model, int year) {
+	cout << "汽车品牌： " << brand << endl;
+	cout << "汽车模型： " << model << endl;
+	cout << "生产年份： " << year << endl;
+}
+
+int main() {
+	Car car("比亚迪", "SUV", 2020);
+	car.display("比亚迪", "SUV", 2020);
+	return 0;
+}
